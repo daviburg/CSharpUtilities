0a5337299617388d20505b77b8f9e6fc18feddb5
David Burg <david.burg@microsoft.com>
2018-07-07 10:26:06 -0700

    NIB: nits in GNU GPL license notice header.

--- Tests/Daviburg.Utilities.Tests/TestEventListener.cs
+++ Tests/Daviburg.Utilities.Tests/TestEventListener.cs
@@ -9,11 +9,11 @@
 //   
 //   Daviburg Utilities is distributed in the hope that it will be useful,
 //   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //   GNU General Public License for more details.
 //
 //   You should have received a copy of the GNU General Public License
-//   along with Foobar.  If not, see<https://www.gnu.org/licenses/>.
+//   along with Daviburg Utilities.  If not, see <see href="https://www.gnu.org/licenses"/>.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
