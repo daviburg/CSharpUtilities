2adf73b3d0289475a43c192d3cc75eb37660f97d
David Burg <david.burg@microsoft.com>
2018-09-05 14:38:29 -0700

    Azure Function v2 re-encode text between any .NET supported encodings.

---
+++ Sources/Daviburg.Utilities/Daviburg.Recode/RecodeFunction.cs
@@ -0,0 +1,82 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RecodeFunction.cs" company="Microsoft">
+//   This file is part of Daviburg Utilities.
+//
+//   Daviburg Utilities is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//   
+//   Daviburg Utilities is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with Daviburg Utilities.  If not, see <see href="https://www.gnu.org/licenses"/>.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Daviburg.Recode
+{
+    using System;
+    using System.IO;
+    using System.Text;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Azure.WebJobs;
+    using Microsoft.Azure.WebJobs.Extensions.Http;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Azure.WebJobs.Host;
+    using Newtonsoft.Json;
+
+    public static class RecodeFunction
+    {
+        [FunctionName("RecodeFunction")]
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
+        {
+            log.Info("C# HTTP trigger function processing a request.");
+
+            Encoding inputEncoding = null;
+
+            string requestBody = new StreamReader(req.Body).ReadToEnd();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+            if (data == null || data.text == null || data.encodingInput == null || data.encodingOutput == null)
+            {
+                return new BadRequestObjectResult("Please pass text/encodingOutput properties in the input Json object.");
+            }
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            try
+            {
+                string encodingInput = data.encodingInput.Value;
+                inputEncoding = Encoding.GetEncoding(name: encodingInput);
+            }
+            catch (ArgumentException)
+            {
+                return new BadRequestObjectResult($"Input char set value '{data.encodingInput.Value}' is not supported. Supported value are listed at https://msdn.microsoft.com/en-us/library/system.text.encoding(v=vs.110).aspx.");
+            }
+
+            Encoding encodingOutput = null;
+            try
+            {
+                string outputEncoding = data.encodingOutput.Value;
+                encodingOutput = Encoding.GetEncoding(outputEncoding);
+            }
+            catch (ArgumentException)
+            {
+                return new BadRequestObjectResult($"Output char set value '{data.encodingOutput.Value}' is not supported. Supported value are listed at https://msdn.microsoft.com/en-us/library/system.text.encoding(v=vs.110).aspx.");
+            }
+
+            string input = data.text;
+            var outputBytes = Encoding.Convert(srcEncoding: inputEncoding, dstEncoding: encodingOutput, bytes: Convert.FromBase64String(input));
+            
+            return (ActionResult)new JsonResult(
+                value: new
+                {
+                    text = Convert.ToBase64String(outputBytes)
+                });
+        }
+    }
+}
